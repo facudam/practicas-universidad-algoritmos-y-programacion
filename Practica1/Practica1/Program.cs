9b74c0f953b70e5eb4125b9c17dc271ac5f11f14
facudam <facundo89caceres@gmail.com>
2024-08-29 20:25:14 -0300

    Crear primera carpeta Practica1

---
+++ Practica1/Practica1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: USURIO
+ * Date: 29/8/2024
+ * Time: 20:23
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Practica1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World! 2");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
