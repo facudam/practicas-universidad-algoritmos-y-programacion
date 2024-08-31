e354d3cb4c22664a555d3094cd50ab7f88dacc2d
facudam <facundo89caceres@gmail.com>
2024-08-30 21:54:55 -0300

    Solución ejercicio 6 Practica 1

---
+++ Practica1/ejercicio6/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Escriba un programa de aplicación que imprima en la consola todos los números impares del intervalo [40,352] que además sean múltiplos de 3.
+ */
+using System;
+
+namespace ejercicio6
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int numeros = 40;
+			
+			while (numeros != 353) {
+				if ((numeros % 2) != 0 && (numeros % 3) == 0) {
+					Console.WriteLine(numeros);
+				}
+				numeros++;
+			}
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
