725a9608e71cbc670c185bebaa8a9886eb598134
facudam <facundo89caceres@gmail.com>
2024-09-04 21:11:28 -0300

    Inicialización de práctica 2 y ejercicio 1

---
+++ Practica2/Ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ ¿Cuál es el problema del código siguiente y cómo se soluciona? Qué valores imprime?
+ 	int i=0; 
+ 	for(int i=1; i<=10;)   
+		{ Console.WriteLine(i++); } 
+	Console.ReadKey();
+ */
+using System;
+
+namespace Ejercicio1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			//int i = 0; Doble declaración.
+			for (int i=1; i<=10;) {
+				Console.WriteLine(i++);
+			}
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
