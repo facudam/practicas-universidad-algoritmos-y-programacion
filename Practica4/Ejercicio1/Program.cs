93f1d8bc95ecca30aee2b2a6db7aa5569b048725
facudam <facundo89caceres@gmail.com>
2024-09-25 20:49:24 -0300

    Práctica 4 ejercicio 1

---
+++ Practica4/Ejercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ Codifique la clase Hora de tal forma que al ejecutar el siguiente programa de aplicación (Main):  class Program { public static void Main(string[] args) { Hora h=new Hora(23,30,15);  
+h.imprimir();  Console.ReadKey(true); } } se imprima por consola:     23 HORAS, 30 MINUTOS Y 15 SEGUNDOS
+ */
+using System;
+using Ejercicio1.clases;
+
+namespace Ejercicio1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Hora hour = new Hora(23, 30, 15);
+			hour.imprimir();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Practica4/Ejercicio1/clases/Hora.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Ejercicio1.clases
+{
+	
+	public class Hora
+	{
+		public Hora(int hora, int minutos, int segundos)
+		{
+			this.hora = hora;
+			this.minutos = minutos;
+			this.segundos = segundos;
+		}
+		
+		private int hora;
+		private int minutos;
+		private int segundos;
+		
+		public void imprimir() {
+			Console.WriteLine("{0} HORAS, {1} MINUTOS Y {2} SEGUNDOS", hora, minutos, segundos);
+		}
+	}
+}
