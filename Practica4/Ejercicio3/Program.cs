fc08e2d95f27b9aa51106e98c3e31a933fae1cbf
facudam <facundo89caceres@gmail.com>
2024-09-25 21:56:42 -0300

    Ejercicio 3 Práctica 4

---
+++ Practica4/Ejercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Modifique la clase anterior agregando un campo privado FechaNacimiento. Defina un nuevo constructor que reciba como parámetro el nombre, la fecha de nacimiento (un DateTime) y el dni. 
+En base a la fecha de nacimiento y la fecha actual debe calcular y guardar la edad.
+ */
+using System;
+using Ejercicio3.clases;
+
+namespace Ejercicio3
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			DateTime fecha1 = new DateTime(2000, 10, 15);
+			Person hombre = new Person("Carlos", fecha1, "45098123");
+			
+			hombre.ImprimirEdad();
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Practica4/Ejercicio3/clases/Person.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace Ejercicio3.clases
+{
+	public class Person
+	{
+		
+		
+		public Person(string nombre, DateTime fechaNacimiento, string dni) {
+			this.nombre = nombre;
+			this.fechaNacimiento = fechaNacimiento;
+			this.dni = dni;
+		}
+		
+		private string nombre;
+		private DateTime fechaNacimiento;
+		private string dni;
+		
+		DateTime fechaActual = DateTime.Now; // No se utiliza el new.
+		
+		public void ImprimirEdad() {
+			int edad = fechaActual.Year - fechaNacimiento.Year;
+			if (DateTime.Compare(fechaActual, fechaNacimiento.AddYears(edad)) < 0) { // DateTime.Compare devuelve un negativo si la primer fecha es menor a la segunda, un 0 si son iguales y un positivo si es mayor.
+				// .AddYears añade años a la fecha que la llame. Para quitar años como argumento le pasamos un numero negativo.
+				edad--;
+			    }
+			Console.WriteLine("{0} tiene {1} años", nombre, edad);
+		}
+	}
+}
