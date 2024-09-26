d998dd115be4e03681308535e6322438ba0a71dc
facudam <facundo89caceres@gmail.com>
2024-09-25 21:01:16 -0300

    Ejercicio 2 practica 4

---
+++ Practica4/Ejercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ Defina la clase Persona con 3 campos: Nombre, Edad y DNI. En el constructor utilice parámetros cuyos nombres coincidan con las variables de instancia del objeto (usar this).  
+ Definir un método que permita imprimir los datos de la persona con el siguiente formato: Nombre (Edad) <TAB> DNI. Ejemplo:  Juan Perez (40)  2098745
+ */
+using System;
+using Ejercicio2.clases;
+
+namespace Ejercicio2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Persona persona1 = new Persona("Juan", 23, "39456521");
+			
+			persona1.showData();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Practica4/Ejercicio2/clases/Persona.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Ejercicio2.clases
+{
+	
+	public class Persona
+	{
+		public Persona(string nombre, int edad, string dni)
+		{
+			this.nombre = nombre;
+			this.edad = edad;
+			this.dni = dni;
+		}
+		private string nombre;
+		private int edad;
+		private string dni;
+		
+		public void showData() {
+			Console.WriteLine("{0} ({1})  {2}", nombre, edad, dni);
+		}
+		
+	}
+}
