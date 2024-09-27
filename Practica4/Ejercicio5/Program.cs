364e1397e791b8458b5adf007bd823c7df11f85c
facudam <facundo89caceres@gmail.com>
2024-09-27 11:26:38 -0300

    Ejercicio 5 de práctica 4

---
+++ Practica4/Ejercicio5/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ Cree una clase llamada Operación con tres variables de instancia: operando1, operando2 y operador. Definir el constructor, todas las propiedades y el método evaluar() que será encargado de efectuar la operación solicitada 
+ según el tipo de operador ( ‘+’: sumar, ‘-‘: restar, ‘*’: multiplicar o ‘/’: dividir, operando 1 y operando 2). El programa de aplicación deberá imprimir todos los resultados solicitados y calculados.  
+ Por ejemplo, si se ejecuta: Operación op=new Operación(3,4,”+”);  Console.WriteLine(op.evaluar()); el programa imprime 7.
+ */
+using System;
+using Ejercicio5.clases;
+
+namespace Ejercicio5
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			Operacion operation = new Operacion(3.5, 4, '+');
+			Console.WriteLine(operation.evaluar());
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Practica4/Ejercicio5/clases/Operacion.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+
+namespace Ejercicio5.clases
+{
+	public class Operacion
+	{
+		public Operacion(double operando1,double operando2, char operador)
+		{
+			this.operando1 = operando1;
+			this.operando2 = operando2;
+			this.operador = operador;
+		}
+		
+		private double operando1;
+		private double operando2;
+		private char operador;
+			
+		public double Operando1 {
+			get {
+				return operando1;
+			}
+			set {
+				operando1 = value;
+			}
+		}
+		
+		public double Operando2 {
+			get {
+				return operando2;
+			}
+			set {
+				operando2 = value;
+			}
+		}
+		
+		public char Operador {
+			get {
+				return operador;
+			}
+			set {
+				operador = value;
+			}
+		}
+		
+		public double evaluar() {
+			double resultado = 0.0;
+			switch(operador) {
+				case '+':
+					resultado = operando1 + operando2;
+					break;
+				case '-':
+					resultado = operando1 - operando2;
+					break;
+				case '*':
+					resultado = operando1 * operando2;
+					break;
+				case '/':
+					resultado = operando1 / operando2;
+					break;
+				default:
+					Console.WriteLine("El operador ingresado es incorrecto");
+					break;
+			}
+			return resultado;
+		}
+	}
+}
