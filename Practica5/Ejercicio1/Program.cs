b1519e7918cb019efcfe8e5c3636c6112c4d1709
facudam <facundo89caceres@gmail.com>
2024-09-30 21:00:11 -0300

    Ejercicio 1 Practica 5

---
+++ Practica5/Ejercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ Implemente la clase horario que permite guardar día, hora y materia a cursar en ese horario
+ */
+using System;
+using Ejercicio1.clases;
+
+namespace Ejercicio1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			// Horario inscripcion = new Horario("Martes", "14:00hs", "Algoritmos y Programación");
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Practica5/Ejercicio1/clases/Horario.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace Ejercicio1.clases
+{
+	
+	public class Horario
+	{
+		private string dia, hora, materia;
+		
+		public Horario(string dia, string hora, string materia)
+		{
+			this.dia = dia;
+			this.hora = hora;
+			this.materia = materia;
+		}
+		
+		public string Dia {
+			get { return dia; }
+			set { dia = value; }
+		}
+		
+		public string Hora {
+			get { return hora; }
+			set { hora = value; }
+		}
+		
+		public string Materia {
+			get { return materia; }
+			set { materia = value; }
+		}
+		
+		
+	}
+}
