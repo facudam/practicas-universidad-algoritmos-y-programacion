24198cdbf337d7973b28e3396eceecde403145d7
facudam <facundo89caceres@gmail.com>
2024-10-06 19:37:30 -0300

    Comenzando ejercicio 2 de práctica 5

---
+++ Practica5/Ejercicio2/Program.cs
@@ -0,0 +1,141 @@
+﻿/*
+ Implemente la clase Alumno para modelar alumnos en una Universidad. Agréguele como estado lo que considere necesario para el problema. Piense ¿cómo implementaría los horarios de cursada de un alumno? 
+(debe usar la clase del ejercicio anterior).
+Como comportamiento implemente el método agregarMateria(string dia, string hora, string materia) que agrega a los horarios del alumno la materia en ese día y hora, si es que tiene libre el horario.
+Piense qué otros métodos podría definir.
+En una aplicación arme una lista de alumnos con sus horarios de cursada. Defina un menú que permita inscribir alumnos en una materia, imprimir los alumnos inscriptos en una materia dada, 
+informar cuántas materias cursa un alumno e imprimir la lista de todos los alumnos.
+Observación: la lista de alumnos puede ser un Array o un ArrayList (no hay que definir otra clase nueva)
+ */
+using System;
+using System.Collections;
+using Ejercicio2.clases;
+
+namespace Ejercicio2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string answer = "s";
+			ArrayList listaDeAlumnos = new ArrayList();
+			
+			do {
+				Console.WriteLine("\nElija la opción que quiera realizar: \n1- Ingresar un nuevo alumno. \n2- Inscribir a un alumno en una materia. \n3- Mostrar alumnos inscritos en una materia. \n4- Mostrar cantidad de materias que cursa un alumno. \n5- Imprimir listado total de alumnos. \n6- Desanotar a un alumno de una materia. \n7- Salir\n");
+				answer = Console.ReadLine();
+				switch(answer) {
+					case "1":
+						addNewStudent(ref listaDeAlumnos);
+						break;
+					case "2":
+						enrollStudent(ref listaDeAlumnos);
+						break;
+					case "3":
+						showStudentsEnrolledInSubjet(listaDeAlumnos);
+						break;
+//					case "4":
+//						showSubjectsListByStudent(listaDeAlumnos, alumno);
+//						break;
+//					case "5":
+//						showTotalListOfStudents(listaDeAlumnos);
+//						break;
+//					case "6":
+//						unEnrollStudentBySubject(ref listaDeAlumnos, alumno, materia);
+//						break;
+					case "7":
+						answer = "7";
+						break;
+					default:
+						Console.WriteLine("\nLa opción ingresada es incorrecta.\n");
+						break;	
+				}
+			} while (answer != "7");
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+		
+		public static void addNewStudent(ref ArrayList listaDeAlumnos) {
+			string nombre, apellido, dni;
+			Console.WriteLine("\nPor favor, ingrese el nombre");
+			nombre = Console.ReadLine();
+			Console.WriteLine("\nAhora ingrese el apellido");
+			apellido = Console.ReadLine();
+			Console.WriteLine("\nIngrese el DNI");
+			dni = Console.ReadLine();
+			
+			if (!estaAlumnoEnLista(listaDeAlumnos, dni)) {
+				Alumno nuevoAlumno = new Alumno(nombre, apellido, dni);
+				listaDeAlumnos.Add(nuevoAlumno);
+				Console.WriteLine("\nEl Alumno {0} {1} se ha registrado correctamente!\n", nombre, apellido);
+			} else {
+				Console.WriteLine("\nYa se encuentra anotado un alumno con el DNI: {0}\n", dni);
+			}	
+		}
+		
+		public static bool estaAlumnoEnLista(ArrayList listaDeAlumnos, string dni) {
+			bool estaYaAnotado = false;
+			foreach(Alumno alumno in listaDeAlumnos) {
+				if(alumno.Dni == dni) {
+					estaYaAnotado = true;
+					break;
+				}
+			}
+			return estaYaAnotado;
+		}
+		
+		public static void enrollStudent(ref ArrayList listaDeAlumnos) {
+			string dni, materia, dia, hora;
+			Console.WriteLine("\nIngrese el DNI del alumno que desea anotar en una materia");
+			dni = Console.ReadLine();
+			bool esAlumno = false;
+			foreach(Alumno alumno in listaDeAlumnos) {
+				if(alumno.Dni == dni) {
+					Console.WriteLine("Ingrese el nombre de la materia.");
+					materia = Console.ReadLine();
+					Console.WriteLine("Ingrese el dia a cursar");
+					dia = Console.ReadLine();
+					Console.WriteLine("Ingrese la hora a cursar");
+					hora = Console.ReadLine();
+					Horario nuevaMateria = new Horario(dia, hora, materia);
+					alumno.agregarMateria(nuevaMateria);
+					esAlumno = true;
+				}
+			}
+			if (!esAlumno) Console.WriteLine("\nNo existe ningún alumno con el DNI: {0}\n", dni);
+		}
+		
+		public static void showStudentsEnrolledInSubjet(ArrayList listaDeAlumnos) {
+			ArrayList listaAlumnosEnMateria = new ArrayList();
+			string materia;
+			Console.WriteLine("Ingrese el nombre de la materia");
+			materia = Console.ReadLine();
+			
+			foreach(Alumno alumno in listaDeAlumnos) {
+				if(estaAlumnoAnotadoEnMateria(alumno, materia)) {
+					listaAlumnosEnMateria.Add(alumno);
+				}
+			}
+			
+			if(listaAlumnosEnMateria.Count != 0) {
+				Console.WriteLine("\nListado de alumnos anotados en la materia {0}:", materia);
+				foreach(Alumno alumno in listaAlumnosEnMateria) {
+					Console.WriteLine("{0}, {1}", alumno.Apellido, alumno.Nombre);
+				}
+			} else {
+				Console.WriteLine("\nNo hay alumnos anotados en la materia {0}\n", materia);
+			}
+		}
+		
+		public static bool estaAlumnoAnotadoEnMateria(Alumno alumno, string materia) {
+			bool estaAnotado = false;
+			foreach(Horario materiaAnotada in alumno.ListaDeHorarios) {
+				if (materiaAnotada.Materia == materia) {
+					estaAnotado = true;
+					break;
+				}
+			}
+			return estaAnotado;
+		}
+	}
+}
---
+++ Practica5/Ejercicio2/clases/Alumno.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Collections;
+
+namespace Ejercicio2.clases
+{
+	/*
+		Implemente la clase Alumno para modelar alumnos en una Universidad. Agréguele como estado lo que considere necesario para el problema. Piense ¿cómo implementaría los horarios de cursada de un alumno? 
+		(debe usar la clase del ejercicio anterior).
+		Como comportamiento implemente el método agregarMateria(string dia, string hora, string materia) que agrega a los horarios del alumno la materia en ese día y hora, si es que tiene libre el horario.
+		Piense qué otros métodos podría definir.
+		En una aplicación arme una lista de alumnos con sus horarios de cursada. Defina un menú que permita inscribir alumnos en una materia, imprimir los alumnos inscriptos en una materia dada, 
+		informar cuántas materias cursa un alumno e imprimir la lista de todos los alumnos.
+		Observación: la lista de alumnos puede ser un Array o un ArrayList (no hay que definir otra clase nueva)
+ 	*/
+	public class Alumno
+	{
+		private string nombre, apellido, dni;
+		private ArrayList listaDeHorarios;
+		
+		public Alumno(string nombre, string apellido, string dni)
+		{
+			this.nombre = nombre;
+			this.apellido = apellido;
+			this.dni = dni;
+			listaDeHorarios = new ArrayList();
+		}
+		
+		public string Nombre {
+			get { return nombre; }
+			set { nombre = value; }
+		}
+		
+		public string Apellido {
+			get { return apellido; }
+			set { apellido = value; }
+		}
+		
+		public string Dni {
+			get { return dni; }
+			set { dni = value; }
+		}
+		
+		public ArrayList ListaDeHorarios {
+			get { return listaDeHorarios; }
+		}
+		
+		public void agregarMateria(Horario nuevaMateria) {
+			bool estaDisponibleElHorario = true;
+			
+			foreach(Horario elemento in listaDeHorarios) {
+				if (elemento.Dia == nuevaMateria.Dia && elemento.Hora == nuevaMateria.Hora) {
+					estaDisponibleElHorario = false;
+					break;
+				}	
+			}
+			if (estaDisponibleElHorario) {
+				listaDeHorarios.Add(nuevaMateria);
+				Console.WriteLine("\nLa inscripción se ha completado con éxito\n");
+			} else {
+				Console.WriteLine("\nLo sentimos pero el horario elegido no está disponible.\n");
+			}
+		}
+		
+		public void eliminarMateria(Horario materia) {
+			bool esMateriaInscripta = false;
+			foreach(Horario elemento in listaDeHorarios) {
+				if (elemento.Dia == materia.Dia && elemento.Hora == materia.Hora && elemento.Materia == materia.Materia) {
+					listaDeHorarios.Remove(materia);
+					Console.WriteLine("La materia {0} se ha eliminado de la lista correctamente", materia.Materia);
+					esMateriaInscripta = true;
+				}
+				
+				if (!esMateriaInscripta) 
+					Console.WriteLine("No se ha encontrado la materia {0}, en el dia y hora indicado", materia.Materia);
+			}
+		}
+		
+		
+		
+	}
+}
