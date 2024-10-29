696320cd7fe871fe246f3364c7ec87f0aca85b42
facudam <facundo89caceres@gmail.com>
2024-10-28 22:05:56 -0300

    Practica 8 recursividad, ejercicio 1

---
+++ Practica8Recursividad/Ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ Implemente una función recursiva que calcule el factorial de un número n, el cual es pasado por parámetro. 
+ */
+using System;
+
+namespace Ejercicio1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			Console.WriteLine(factorial(5));
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+		
+		public static int factorial(int n) {
+			if (n == 1) {
+				return 1;
+			} 
+			return n * factorial(n - 1);
+			
+		}
+	}
+}
