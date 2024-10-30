135205fcd940b96ba12e09b27f8ae42c67dd87e7
facudam <facundo89caceres@gmail.com>
2024-10-29 21:28:59 -0300

    Ejercicio 4 de práctica 8

---
+++ Practica8Recursividad/Ejercicio4/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ Escriba una función recursiva que reciba un string y retorne la cantidad de letras ‘c’ que contiene.
+ */
+using System;
+
+namespace Ejercicio4
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(cantidadDeLetrasCEn("Facundo Cáceres"));
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+		
+		public static int cantidadDeLetrasCEn(string palabra, int posActual = 0) {
+			palabra = palabra.ToLower();
+			
+			if (posActual == palabra.Length - 1) {
+				if (palabra[posActual] == 'c') return 1;
+				return 0;
+			}
+			
+			if (palabra[posActual] == 'c') {
+				return 1 + cantidadDeLetrasCEn(palabra, posActual + 1);
+			} 
+			
+			return 0 + cantidadDeLetrasCEn(palabra, posActual + 1);
+			
+		}
+	}
+}
