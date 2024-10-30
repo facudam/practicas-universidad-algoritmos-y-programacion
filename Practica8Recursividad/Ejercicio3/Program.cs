941e5a1772325b89fa5dbc00b1d056ef88f00b4b
facudam <facundo89caceres@gmail.com>
2024-10-29 21:08:09 -0300

    Ejercicio 3 de practica 8

---
+++ Practica8Recursividad/Ejercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ 	Escriba una función recursiva que reciba un arreglo de números y devuelva el promedio. 
+ */
+using System;
+
+namespace Ejercicio3
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int[] arreglo = {1,3,5,4,8};
+		
+			Console.WriteLine(calcularPromedio(arreglo));
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+		
+		public static double calcularPromedio(int[] arreglo, int indice = 0)
+		{
+		    if (indice == arreglo.Length - 1)
+		        return arreglo[indice] / (double)arreglo.Length;
+		    
+		    return (arreglo[indice] / (double)arreglo.Length) + calcularPromedio(arreglo, indice + 1);
+		}
+		
+	}
+}
